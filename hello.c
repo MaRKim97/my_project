2af666381389119a91b70ca4feb54a9e70cae49e
Maro Kim <rlaakfh97@gmail.com>
2024-03-15 10:50:28 +0900

    conflict issue

--- hello.c
+++ hello.c
@@ -2,8 +2,13 @@
 
 int main(void)
 {
+<<<<<<< HEAD
     printf("Hello World!");
     printf("This is 2nd line.");
     return 0;
+=======
+print("Hello World!");
+return 0;
+>>>>>>> parent of 67daac8 (2nd)
 }
 
